b41d325093c769a41dc6248efbc2d474a9523ca0
relESCampONes <wellington_lima@live.com>
2023-05-15 21:35:09 -0300

    Aprendendo metodos Get e Set

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,9 +1,43 @@
 public class Jogador {
-
+    public int kak = 0;
+    private final int MAX_VIDAS = 5;
     private int number = 0;
+    private int vidas = 0;
+    private int vidaExtra = 0;
+    private int vidaRestante = 0;
     
     public Jogador(int number){
         this.number = number; // Para fazer referência ao atributo number da classe é necessário inserir "this."
-        System.out.printf("Jogador número %d criado%n", number);
+        System.out.printf("Jogador número %d criado%n", this.number);
     }
+
+    public int getVidas(){
+        return this.vidas;
+    }
+
+    public void setVidas(int vidas){
+        if(vidas > MAX_VIDAS){
+            this.vidas = MAX_VIDAS;
+        }
+        else if(vidas < 0){
+            this.vidas = 0;
+        }
+        else{
+            this.vidas = vidas;
+        }
+    }
+
+    public void addVidas(int vidaExtra){
+        this.vidaExtra = vidaExtra;
+        this.vidaRestante = MAX_VIDAS - this.vidas;
+        if((this.vidas + this.vidaExtra) < 0){
+            this.vidas = 0;
+        }else if(vidaRestante >= 0 && this.vidaExtra <= vidaRestante){
+            this.vidas = this.vidas + this.vidaExtra;
+        }else{            
+            vidas = MAX_VIDAS;
+        }
+
+    }
+
 }
---
+++ src/MetodosGetSet.java
@@ -0,0 +1,33 @@
+public class MetodosGetSet {
+    public static void main(String[] args) {
+        
+        int num = 0;
+
+        Jogador j1 = new Jogador(++num);
+        Jogador j2 = new Jogador(++num);
+        Jogador j3 = new Jogador(++num);
+
+        j1.setVidas(4);
+        j2.setVidas(3);
+        j3.setVidas(2);
+
+        j1.addVidas(-3);
+        j2.addVidas(-1);
+        j3.addVidas(3);
+
+        System.out.printf("%nVidas do jogador 1: %d",j1.getVidas());
+        System.out.printf("%nVidas do jogador 2: %d",j2.getVidas());
+        System.out.printf("%nVidas do jogador 3: %d",j3.getVidas());
+ 
+
+        // System.out.printf("%nVidas do jogador 1: %d",j1.num);
+            
+
+
+
+    }    
+
+
+
+
+}
