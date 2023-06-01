0c1b91d97f563ccd35e4a5e79fab6b6c1fc154d3
relESCampONes <wellington_lima@live.com>
2023-06-01 20:29:25 -0300

    learning about static

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,12 +1,13 @@
 public class Jogador {
-    public int kak = 0;
     private final int MAX_VIDAS = 5;
     private int number = 0;
     private int vidas = 0;
     private int vidaExtra = 0;
     private int vidaRestante = 0;
-    
+    static int quantidadeTotalJogadores = 0;
+
     public Jogador(int number){
+        quantidadeTotalJogadores ++;
         this.number = number; // Para fazer referência ao atributo number da classe é necessário inserir "this."
         System.out.printf("Jogador número %d criado%n", this.number);
     }
@@ -35,9 +36,10 @@ public class Jogador {
         }else if(vidaRestante >= 0 && this.vidaExtra <= vidaRestante){
             this.vidas = this.vidas + this.vidaExtra;
         }else{            
-            vidas = MAX_VIDAS;
+            this.vidas = MAX_VIDAS;
         }
 
     }
+
 
 }
--- src/MetodosGetSet.java
+++ src/MetodosGetSet.java
@@ -6,21 +6,29 @@ public class MetodosGetSet {
         Jogador j1 = new Jogador(++num);
         Jogador j2 = new Jogador(++num);
         Jogador j3 = new Jogador(++num);
+        Jogador j4 = new Jogador(++num);
 
-        j1.setVidas(0);
-        j2.setVidas(0);
+        j1.setVidas(5);
+        j2.setVidas(3);
         j3.setVidas(0);
+        j4.setVidas(0);
 
         j1.addVidas(-3);
         j2.addVidas(-1);
         j3.addVidas(3);
+        j4.addVidas(2);
 
-        System.out.printf("%nVidas do jogador 1: %d",j1.getVidas());
-        System.out.printf("%nVidas do jogador 2: %d",j2.getVidas());
-        System.out.printf("%nVidas do jogador 3: %d",j3.getVidas());
- 
+        num = 0;
+
+        System.out.printf("%nVidas do jogador %d/%d: %d",++num,Jogador.quantidadeTotalJogadores,j1.getVidas());
+        System.out.printf("%nVidas do jogador %d/%d: %d",++num,Jogador.quantidadeTotalJogadores,j2.getVidas());
+        System.out.printf("%nVidas do jogador %d/%d: %d",++num,Jogador.quantidadeTotalJogadores,j3.getVidas());
+        System.out.printf("%nVidas do jogador %d/%d: %d",++num,Jogador.quantidadeTotalJogadores,j4.getVidas());
 
         // System.out.printf("%nVidas do jogador 1: %d",j1.num);
+        // Atributos=propriedades=variaveis
+        // Métodos=funções
+
             
 
 
