29248adeaa10a0b503390a27d52540d41dd5b2f7
relESCampONes <wellington_lima@live.com>
2023-06-08 21:54:01 -0300

    fechando o scanner

--- src/ObterDadosTeclado.java
+++ src/ObterDadosTeclado.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ObterDadosTeclado {
 
     public static void main(String[] args) {
-    
+
         Scanner entrada = new Scanner(System.in);
         float n1=0,n2=0,n3=0,n4=0;
         double mediaAluno=0;
@@ -15,7 +15,7 @@ public class ObterDadosTeclado {
         System.out.print("Digite a quantidade de alunos que terminaram todas as provas: ");
         quantidadeAluno=entrada.nextInt();
 
-        // for(int cont=0 ; cont<=3 ; cont++){  
+        // for(int cont=0 ; cont<=3 ; cont++){
         while(cont<quantidadeAluno){
             System.out.print("\nDigite o nome do aluno: ");
             nome=entrada.next();
@@ -28,16 +28,18 @@ public class ObterDadosTeclado {
             System.out.print("Digite a quarta nota: ");
             n4=entrada.nextFloat();
             mediaAluno=(n1+n2+n3+n4)/4;
-        
+
             if(mediaAluno<MEDIA_NOTA){
                 res="REPROVADO";
             }else if(mediaAluno>=MEDIA_NOTA){
                 res="APROVADO";
             }
-        
+
+            entrada.close();
+
             System.out.printf("A Média da faculdade é: %.2f e %s teve a nota: %.2f, portanto está %s!!!%n",MEDIA_NOTA,nome,mediaAluno,res);
             cont++;
-    
+
        }
     }
 }
